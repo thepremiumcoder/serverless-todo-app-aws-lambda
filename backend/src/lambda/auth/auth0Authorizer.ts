5a2eb5fec9c3af99e7c796e5311c2d717b4fc430
thepremiumcoder <thepremiumcoder@gmail.com>
2023-03-07 19:08:36 +0100

    Authentication: Verify Token

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -6,13 +6,14 @@ import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
+import { Response } from 'aws-sdk'
 
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://premiumcoder-342.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -55,13 +56,40 @@ export const handler = async (
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  try {
+    const token = getToken(authHeader)
+    const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+    // TODO: Implement token verification
+    // You should implement it similarly to how it was implemented for the exercise for the lesson 5
+    // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
+    // return undefined
+
+    const authData = await Axios.get(jwksUrl)
+    const authKeys = authData?.data?.keys
+    const jwtSigningKeys = authKeys?.find((key) => key.id === jwt?.header?.kid)
+
+    //Activity  Log ::  Attempt findindg Signing Key
+    logger.info('Fetch Signing Key', jwtSigningKeys)
+
+    if (!jwtSigningKeys) {
+      logger.error('Signing Key Not Found', 'No Matching Signing Key Found')
+      throw new Error('No Matching Signing Key Found')
+    }
+
+    // FETCH PEM
+    const pem = jwtSigningKeys.x5x[0]
+
+    // CONVERT PEM TO CERTIFICATE
+    const certificate = `-----BEGIN CERTIFICATE-----\n${pem}\n-----END CERTIFICATE-----`
+
+    // RETURN VERIFICATION TOKEN
+    return verify(token, certificate, { algorithms: ['RS256'] }) as JwtPayload
+  } catch (error) {
+    //Activity  Log ::  Log Token Verification Error
+    logger.error('Token Verification failed', error)
+    throw new Error('Token Verification was not successful')
+  }
 }
 
 function getToken(authHeader: string): string {
